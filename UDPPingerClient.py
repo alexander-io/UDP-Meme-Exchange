cad752a20620ad3bc1d397d5315798ef6c7e1185
David Olson <David Olson>
2017-05-05 22:39:53 -0700

    changes to client

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -3,7 +3,7 @@ from socket import *
 
 # build socket\
 # FIXME - input the ip address of the server rather than local host
-serverName = 'localhost'
+serverName = '10.150.26.221'
 serverPort = 12000
 
 # establish the udp socket
@@ -13,26 +13,37 @@ for x in range(1,10): # do from 1 to 10
     try:
 
         # determine the meme message to send
-        message = 'meme:' + 'http://opensource.eco'
+        message = 'meme@' + 'http://opensource.eco'
 
         clientSocket.sendto(message.encode(), (serverName, serverPort))
 
         # After the message has been sent, set the timeout.
-        clientSocket.settimeout(1)  # sets a timeout of one second.
+        clientSocket.settimeout(.01)  # sets a timeout of one second.
                                     # This will be caught in the
                                     # except block below.
-
+        print("before recv")
 
         # determine the return message & receive it
+
+        returned_message = clientSocket.recv(1024)
+
+
         # returned_message, serverAddress = (
-        #     clientSocket.recvfrom(2048)
+        #     clientSocket.recv(1024)
         # )
+
+        print("before decode")
         # # decode the return message from the server
-        # decoded_message = returned_message.decode()
+        decoded_message = returned_message.decode('utf-8')
+        print("here is the message",decoded_message)
+        if 'ack' in decoded_message:
+            print("got ack")
+            break
+
         # decoded_message = decoded_message.split(' ')
         # print('return message : ', returned_message.decode(), ' server address : ', serverAddress)
 
-    except err:
-        print('err')
+    except:
+        pass
 
 clientSocket.close()
